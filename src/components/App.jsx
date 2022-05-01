dd1589ce411e0f0586d3042c20d40bd4c8a6cca3
Adiiiiti <96163292+Adiiiiti@users.noreply.github.com>
2022-05-01 18:09:21 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./login";
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
